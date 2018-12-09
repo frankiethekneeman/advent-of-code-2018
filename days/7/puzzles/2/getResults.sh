fc060a578c5eb18a41722d7a8e8432bace521024
Francis Sirizzotti <francis@sirizzotti.info>
2018-12-09 10:43:36 -0500

    refining getAllSolutions

--- days/7/puzzles/2/getResults.sh
+++ days/7/puzzles/2/getResults.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 scalac Program.scala
 scala Program < input 
-rm *.class
--- getAllSolutions.sh
+++ getAllSolutions.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+runs=${1:-1}
 for day in {1..25}
 do
     for puzzle in 1 2 
@@ -6,12 +8,16 @@ do
         target="days/$day/puzzles/$puzzle/"
         if [ -d $target ]
         then
-            cd $target
-            echo -n "-> "
-            ./getResults.sh
-            cd -
+            cd $target 2>&1 >/dev/null
+            for run in $(seq 1 $runs)
+            do
+                echo "   Run $run of $runs"
+                # Indent 5 spaces because time uses tabs to align
+                (time ./getResults.sh) 2>&1 | sed 's/^/     /'
+            done
+            cd - 2>&1 >/dev/null
         else
-            echo "-> Puzzle not yet solved."
+            echo "   Puzzle not yet solved."
         fi
     done
 done
